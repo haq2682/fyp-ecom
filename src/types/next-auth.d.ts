24269e9d917ee88edc2828062a68e367c8edffcd
Abdul Haq Khalid <haq2682@gmail.com>
2024-12-02 23:40:06 +0500

    Fixed migrations

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth, { DefaultSession, DefaultJWT } from "next-auth"
+import { User } from "@prisma/client"
+
+declare module "next-auth" {
+    interface Session extends DefaultSession {
+        user: User
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT extends DefaultJWT, Omit<User, "id"> {
+        id: number
+    }
+}
+
